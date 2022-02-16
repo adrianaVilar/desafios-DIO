dc05668525b119f9e5c4de220bc65a973e0d3efc
adrianaVilar <adriana.correa@outlook.com>
2022-02-16 18:40:06 -0300

    projeto

--- java/src/com/dio/MyFirstProject.java
+++ java/src/com/dio/MyFirstProject.java
@@ -2,6 +2,13 @@ package com.dio;
 
 import com.dio.base.Order;
 
+/**
+ * @author Adriana Vilar
+ * @version 1.0.0
+ * @see MyFirstProject
+ * @since Release 1.0.0
+ */
+
 public class MyFirstProject {
 
     public static void main(String[] args) {
